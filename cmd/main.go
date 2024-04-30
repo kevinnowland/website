3778719aec216dd0b16f3d62c430bb85dd3ab7e3
Kevin Nowland <kevin.nowland@gmail.com>
2024-04-29 21:52:40 -0400

    feat: make logger configurable by WEBSITE_LOG_LEVEL env var

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,32 @@ import (
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	logger.Info("logger initialized")
+	logger := getLogger()
 
 	logger.Info("hello, world!")
+}
+
+func getLogger() *slog.Logger {
+	var logLevel slog.Level
+	switch l := os.Getenv("WEBSITE_LOG_LEVEL"); l {
+	case "DEBUG":
+		logLevel = slog.LevelDebug
+	case "INFO":
+		logLevel = slog.LevelInfo
+	case "WARN":
+		logLevel = slog.LevelWarn
+	case "ERROR":
+		logLevel = slog.LevelError
+	default:
+		logLevel = slog.LevelInfo
+	}
+
+	logOpts := &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: true,
+	}
+	handler := (slog.NewJSONHandler(os.Stdout, logOpts))
+	logger := slog.New(handler)
+
+	return logger
 }
